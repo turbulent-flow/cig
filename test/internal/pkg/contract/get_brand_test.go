bab242d9952c90eb1e72bb6630637ae3d1b854a7
liam <hello@mengliu.dev>
2025-04-09 15:12:07 +0800

    feat: add testing for contract.GetBrand()

---
+++ test/internal/pkg/contract/get_brand_test.go
@@ -0,0 +1,37 @@
+package contract
+
+import (
+	"cig/pkg/helper"
+	"cig/test/support/config"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+// case: It should return an error with empty id.
+func TestGetBrandWithEmptyId(t *testing.T) {
+	ec := config.DefaultEthConfig
+	ec.BeforeTest(t)
+	defer ec.Backend.Close()
+
+	var emptyBytes32Id [32]byte
+	_, err := ec.ContractInstance.GetBrand(nil, emptyBytes32Id)
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "Brand ID is invalid")
+	}
+}
+
+// case: It should return an error with invalid id.
+func TestGetBrandWithInvalidId(t *testing.T) {
+	ec := config.DefaultEthConfig
+	ec.BeforeTest(t)
+	defer ec.Backend.Close()
+
+	id := uuid.New()
+	invalidBytes32Id := helper.UuidToBytes32(id)
+	_, err := ec.ContractInstance.GetBrand(nil, invalidBytes32Id)
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "Brand does not exist")
+	}
+}
